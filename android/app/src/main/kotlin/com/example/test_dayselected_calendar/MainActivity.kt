64478f0c81cbe8a5eea0893d01a805ac9e7fe74c
toyohide <hide.toyoda@gmail.com>
2022-07-30 22:37:02 +0900

    flutter table calendar

---
+++ android/app/src/main/kotlin/com/example/test_dayselected_calendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_dayselected_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/calendar.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime _focusedDay = DateTime.now();
+
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 50),
+          TableCalendar(
+            calendarStyle: const CalendarStyle(
+              todayDecoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Colors.indigo,
+                shape: BoxShape.circle,
+              ),
+
+              ///
+
+              rangeHighlightColor: Color(0xFFBBDDFF),
+
+              ///
+
+              todayTextStyle: TextStyle(color: Color(0xFFFAFAFA)),
+              selectedTextStyle: TextStyle(color: Color(0xFFFAFAFA)),
+              rangeStartTextStyle: TextStyle(color: Color(0xFFFAFAFA)),
+              rangeEndTextStyle: TextStyle(color: Color(0xFFFAFAFA)),
+//              outsideTextStyle: TextStyle(color: Colors.white),
+              disabledTextStyle: TextStyle(color: Colors.grey),
+              holidayTextStyle: TextStyle(color: Color(0xFF5C6BC0)),
+              weekendTextStyle: TextStyle(color: Colors.white),
+
+              ///
+
+              markerDecoration: BoxDecoration(color: Colors.white),
+              rangeStartDecoration: BoxDecoration(color: Color(0xFF6699FF)),
+              rangeEndDecoration: BoxDecoration(color: Color(0xFF6699FF)),
+              holidayDecoration: BoxDecoration(
+                border: Border.fromBorderSide(
+                  BorderSide(
+                    color: Color(0xFF9FA8DA),
+                  ),
+                ),
+              ),
+            ),
+
+            ///
+
+            headerStyle: const HeaderStyle(formatButtonVisible: false),
+            firstDay: DateTime.utc(2020, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+
+            ///
+
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
